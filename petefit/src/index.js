1715dbfc555454a85ce216646136299cc3c8f662
hayoon07 <backhay0714@naver.com>
2024-07-23 10:33:08 +0900

    :truck:Remove: 헤더, 푸터 폴더 삭제

--- petefit/src/index.js
+++ petefit/src/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import Header from "./components/header/Header";
-import Footer from "./components/footer/Footer";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import Rank from "./components/Rank";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
