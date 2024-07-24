81b749c2dea348877b26db958fc5b29f11b69924
hs0333 <huiseoj6@gmail.com>
2024-07-24 17:48:18 +0900

    :ambulance: Chore. index.js 파일 수정

--- petefit/src/index.js
+++ petefit/src/index.js
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import Header from "./components/header/Header";
-import Footer from "./components/footer/Footer";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import Calendar from "./components/calendar/Calendar";
+import Rank from "./components/Rank";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
